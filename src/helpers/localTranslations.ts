8ffe1f36d90bc667d0c28fa19deb9eaed4443b8c
Alejandro Sauqué Pérez <alejandro.sauqueprz@uanl.edu.mx>
2025-04-07 23:51:08 -0600

    se agregan traducciones locales en ts

---
+++ src/helpers/localTranslations.ts
@@ -0,0 +1,21 @@
+export const emailModeTranslation = {
+  daily: 'Diario',
+  'some-days': 'Algunos días',
+  unique: 'Único',
+}
+
+export const addresseeModeTranslation = {
+  all: 'Todos',
+  some: 'Algunos',
+  percent: 'Porcentaje aleatorio',
+}
+
+export const weekDaysTranslations = {
+  monday: 'Lunes',
+  tuesday: 'Martes',
+  wednesday: 'Miercoles',
+  thursday: 'Jueves',
+  friday: 'Viernes',
+  saturday: 'Sábado',
+  sunday: 'Domingo',
+}
