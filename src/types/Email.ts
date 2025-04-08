9d2617509b47e43f016129efe90eb233dad3ae4b
Alejandro Sauqué Pérez <alejandro.sauqueprz@uanl.edu.mx>
2025-04-07 23:50:25 -0600

    Se agrega el type de email

--- src/types/Email.ts
+++ src/types/Email.ts
@@ -1,4 +1,4 @@
-import type { DocumentSnapshot } from 'firebase/firestore'
+import type { QueryDocumentSnapshot } from 'firebase/firestore'
 import type { WeekDay } from './Time'
 
 export type NewEmail = Omit<Email, 'id'>
@@ -8,7 +8,7 @@ export type Email = {
   title: string
   subject: string
   body: string
-  filesIds: string[]
+  filesUrls: string[]
   mode: 'daily' | 'some-days' | 'unique'
   days: WeekDay[] | null
   sendingDay: Date | null
@@ -19,20 +19,20 @@ export type Email = {
   createdAt: Date
 }
 
-// export function emailsFromFirestore(doc: DocumentSnapshot): Email {
-//   return {
-//     id: doc.id,
-//   title: doc.data().title,
-//   subject: string,
-//   body: string,
-//   filesIds: string[],
-//   mode: 'daily' | 'some-days' | 'unique',
-//   days: WeekDay[] | null,
-//   sendingDay: Date | null,
-//   sendingHour: string,
-//   addresseeMode: 'all' | 'some' | 'percent',
-//   addresseeEmails: string[] | null,
-//   addresseePercent: number | null,
-//   createdAt: Date,
-//   }
-// }
+export function emailFromFirestore(doc: QueryDocumentSnapshot): Email {
+  return {
+    id: doc.id,
+    title: doc.data().title,
+    subject: doc.data().subject,
+    body: doc.data().body,
+    filesUrls: doc.data().filesUrls,
+    mode: doc.data().mode,
+    days: doc.data().days,
+    sendingDay: doc.data().sendingDay ? doc.data().sendingDay.toDate() : null,
+    sendingHour: doc.data().sendingHour,
+    addresseeMode: doc.data().addresseeMode,
+    addresseeEmails: doc.data().addresseeEmails,
+    addresseePercent: doc.data().addresseePercent,
+    createdAt: doc.data().createdAt ? doc.data().createdAt.toDate() : null,
+  }
+}
